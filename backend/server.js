c9b8a2c4d8d6d6e932d7b03cdeaf9abb2b84abce
Gh0stbasta <ubuntuvj@gmail.com>
2025-05-16 22:13:33 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,21 @@ const sqlite3 = require("sqlite3");
 const server = express();
 
 //db
-const db = new sqlite3.Database("./gameDB.sqlite");
-db.run("DROP TABLE IF EXISTS leaderboard");
-db.run(
-  `CREATE TABLE IF NOT EXISTS leaderboard (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                score INTEGER NOT NULL
-        )`
-);
+const db = new sqlite3.Database("./gameDB.sqlite", (err) => {
+  if (err) {
+    console.error("Error opening database " + err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run("DROP TABLE IF EXISTS leaderboard");
+    db.run(`
+      CREATE TABLE IF NOT EXISTS leaderboard (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        score INTEGER NOT NULL
+      )
+    `);
+  }
+});
 
 //middleware
 server.use(express.json());
